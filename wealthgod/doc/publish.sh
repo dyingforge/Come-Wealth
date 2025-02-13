cd52ceb3eef325299501c78dac5ae4d9e38091a3
dyingforge <1289470395.@qq.com>
2025-02-13 13:33:25 +0800

    初版

---
+++ wealthgod/doc/publish.sh
@@ -0,0 +1,144 @@
+Transaction Digest: GJiSpujNEhVQXjKUu3HVxYCYsp8c8c7XDPgU5vpDQCsD
+╭──────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Transaction Data                                                                                             │
+├──────────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Sender: 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294                                   │
+│ Gas Owner: 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294                                │
+│ Gas Budget: 20004400 MIST                                                                                    │
+│ Gas Price: 1000 MIST                                                                                         │
+│ Gas Payment:                                                                                                 │
+│  ┌──                                                                                                         │
+│  │ ID: 0x9d106142c653d072ca6b1c2534e925b8ca62fe349f83310719cb517d0fa8ba70                                    │
+│  │ Version: 341948340                                                                                        │
+│  │ Digest: 2pinupXjkwcUjGEaAqnrmLCZkHYdrM2ECmNzU3BELStd                                                      │
+│  └──                                                                                                         │
+│                                                                                                              │
+│ Transaction Kind: Programmable                                                                               │
+│ ╭──────────────────────────────────────────────────────────────────────────────────────────────────────────╮ │
+│ │ Input Objects                                                                                            │ │
+│ ├──────────────────────────────────────────────────────────────────────────────────────────────────────────┤ │
+│ │ 0   Pure Arg: Type: address, Value: "0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294" │ │
+│ ╰──────────────────────────────────────────────────────────────────────────────────────────────────────────╯ │
+│ ╭─────────────────────────────────────────────────────────────────────────╮                                  │
+│ │ Commands                                                                │                                  │
+│ ├─────────────────────────────────────────────────────────────────────────┤                                  │
+│ │ 0  Publish:                                                             │                                  │
+│ │  ┌                                                                      │                                  │
+│ │  │ Dependencies:                                                        │                                  │
+│ │  │   0x0000000000000000000000000000000000000000000000000000000000000001 │                                  │
+│ │  │   0x0000000000000000000000000000000000000000000000000000000000000002 │                                  │
+│ │  └                                                                      │                                  │
+│ │                                                                         │                                  │
+│ │ 1  TransferObjects:                                                     │                                  │
+│ │  ┌                                                                      │                                  │
+│ │  │ Arguments:                                                           │                                  │
+│ │  │   Result 0                                                           │                                  │
+│ │  │ Address: Input  0                                                    │                                  │
+│ │  └                                                                      │                                  │
+│ ╰─────────────────────────────────────────────────────────────────────────╯                                  │
+│                                                                                                              │
+│ Signatures:                                                                                                  │
+│    RtzIPmPEtbCniqptqrh/WzEui81eqiSHang4pJaxGf79NFYbDFwfylly+iN8Uvsm4T9CipSIgEjOxXKdQkg3DA==                  │
+│                                                                                                              │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭───────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Transaction Effects                                                                               │
+├───────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Digest: GJiSpujNEhVQXjKUu3HVxYCYsp8c8c7XDPgU5vpDQCsD                                              │
+│ Status: Success                                                                                   │
+│ Executed Epoch: 644                                                                               │
+│                                                                                                   │
+│ Created Objects:                                                                                  │
+│  ┌──                                                                                              │
+│  │ ID: 0x00912aab0fed27bcd9d261ffe1771b3ccaa97ad7607471bf508a5c07abbbb05c                         │
+│  │ Owner: Account Address ( 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294 )  │
+│  │ Version: 341948341                                                                             │
+│  │ Digest: 8DcnSd7a6zZbDQGDb1FLxoEUMuPo752bBtFL2YBPXUtR                                           │
+│  └──                                                                                              │
+│  ┌──                                                                                              │
+│  │ ID: 0x48bd473a6b61d37a0a1c50c93d47152d16f2e6e04e9f43198a9a36074c5e9144                         │
+│  │ Owner: Shared( 341948341 )                                                                     │
+│  │ Version: 341948341                                                                             │
+│  │ Digest: Fxj7a7B1aeWUMHYaWyaW7oBUstxnorh6mLoLviWTFdP1                                           │
+│  └──                                                                                              │
+│  ┌──                                                                                              │
+│  │ ID: 0x941a3db9b686dd3b77aa21836c9981026919959398ebb7eab5cfd040224f246a                         │
+│  │ Owner: Immutable                                                                               │
+│  │ Version: 1                                                                                     │
+│  │ Digest: Fj5hFTsrVarPiLHNpoCzmco654LGcauTNYSmoG5ZsFsi                                           │
+│  └──                                                                                              │
+│ Mutated Objects:                                                                                  │
+│  ┌──                                                                                              │
+│  │ ID: 0x9d106142c653d072ca6b1c2534e925b8ca62fe349f83310719cb517d0fa8ba70                         │
+│  │ Owner: Account Address ( 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294 )  │
+│  │ Version: 341948341                                                                             │
+│  │ Digest: 6JFzpQ88GJ8FHvseijdTarmd8TumyTow8RdCfKna8FTq                                           │
+│  └──                                                                                              │
+│ Gas Object:                                                                                       │
+│  ┌──                                                                                              │
+│  │ ID: 0x9d106142c653d072ca6b1c2534e925b8ca62fe349f83310719cb517d0fa8ba70                         │
+│  │ Owner: Account Address ( 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294 )  │
+│  │ Version: 341948341                                                                             │
+│  │ Digest: 6JFzpQ88GJ8FHvseijdTarmd8TumyTow8RdCfKna8FTq                                           │
+│  └──                                                                                              │
+│ Gas Cost Summary:                                                                                 │
+│    Storage Cost: 18004400 MIST                                                                    │
+│    Computation Cost: 1000000 MIST                                                                 │
+│    Storage Rebate: 978120 MIST                                                                    │
+│    Non-refundable Storage Fee: 9880 MIST                                                          │
+│                                                                                                   │
+│ Transaction Dependencies:                                                                         │
+│    2wu9cbcnDKziHyDjEwSjTwsSCqZV3eLM4ZPCTfLXeRuD                                                   │
+│    5XvhL1EeEbZcMfKaGHzkiVvbkVk1DM7Adp4cMbku4gQ4                                                   │
+│    Gtwgse64nSVXhQvmqCpwCe5xJz9N4VypvEGJUy5DyG4e                                                   │
+╰───────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭─────────────────────────────╮
+│ No transaction block events │
+╰─────────────────────────────╯
+
+╭──────────────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Object Changes                                                                                           │
+├──────────────────────────────────────────────────────────────────────────────────────────────────────────┤
+│ Created Objects:                                                                                         │
+│  ┌──                                                                                                     │
+│  │ ObjectID: 0x00912aab0fed27bcd9d261ffe1771b3ccaa97ad7607471bf508a5c07abbbb05c                          │
+│  │ Sender: 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294                            │
+│  │ Owner: Account Address ( 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294 )         │
+│  │ ObjectType: 0x2::package::UpgradeCap                                                                  │
+│  │ Version: 341948341                                                                                    │
+│  │ Digest: 8DcnSd7a6zZbDQGDb1FLxoEUMuPo752bBtFL2YBPXUtR                                                  │
+│  └──                                                                                                     │
+│  ┌──                                                                                                     │
+│  │ ObjectID: 0x48bd473a6b61d37a0a1c50c93d47152d16f2e6e04e9f43198a9a36074c5e9144                          │
+│  │ Sender: 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294                            │
+│  │ Owner: Shared( 341948341 )                                                                            │
+│  │ ObjectType: 0x941a3db9b686dd3b77aa21836c9981026919959398ebb7eab5cfd040224f246a::wealthgod::WeathPool  │
+│  │ Version: 341948341                                                                                    │
+│  │ Digest: Fxj7a7B1aeWUMHYaWyaW7oBUstxnorh6mLoLviWTFdP1                                                  │
+│  └──                                                                                                     │
+│ Mutated Objects:                                                                                         │
+│  ┌──                                                                                                     │
+│  │ ObjectID: 0x9d106142c653d072ca6b1c2534e925b8ca62fe349f83310719cb517d0fa8ba70                          │
+│  │ Sender: 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294                            │
+│  │ Owner: Account Address ( 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294 )         │
+│  │ ObjectType: 0x2::coin::Coin<0x2::sui::SUI>                                                            │
+│  │ Version: 341948341                                                                                    │
+│  │ Digest: 6JFzpQ88GJ8FHvseijdTarmd8TumyTow8RdCfKna8FTq                                                  │
+│  └──                                                                                                     │
+│ Published Objects:                                                                                       │
+│  ┌──                                                                                                     │
+│  │ PackageID: 0x941a3db9b686dd3b77aa21836c9981026919959398ebb7eab5cfd040224f246a                         │
+│  │ Version: 1                                                                                            │
+│  │ Digest: Fj5hFTsrVarPiLHNpoCzmco654LGcauTNYSmoG5ZsFsi                                                  │
+│  │ Modules: wealthgod                                                                                    │
+│  └──                                                                                                     │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+╭───────────────────────────────────────────────────────────────────────────────────────────────────╮
+│ Balance Changes                                                                                   │
+├───────────────────────────────────────────────────────────────────────────────────────────────────┤
+│  ┌──                                                                                              │
+│  │ Owner: Account Address ( 0x9222bc4b61099ced2ab5719c6528567ff75dafbfa3cddf9e2078e5c733dd3294 )  │
+│  │ CoinType: 0x2::sui::SUI                                                                        │
+│  │ Amount: -18026280                                                                              │
+│  └──                                                                                              │
+╰───────────────────────────────────────────────────────────────────────────────────────────────────╯
--- wealthgod/sources/wealthgod.move
+++ wealthgod/sources/wealthgod.move
@@ -12,25 +12,28 @@ module wealthgod::wealthgod {
   use sui::sui::SUI;
   use sui::event;
   use sui::random::{Random,Self};
-//strcut
-  public struct WealthGod has key{
-        id: UID,
-        sender: address,
-        isclaimed: bool,
-        amount: Balance<SUI>,
-        image: String,
-    }
+  use sui::table::{Self, Table};
 
-public struct User has key{
+  const EProfileExist: u64 = 2;
+//strcut
+public struct WealthGod has key{
+    id: UID,
+    sender: address,
+    isclaimed: bool,
+    amount: Balance<SUI>,
+}
+
+public struct State has key{
+    id: UID,
+    //<owner,profile>
+    profiles: Table<address, address>,
+}
+
+public struct Profile has key,store{
     id: UID,
     sendAmount: u64,
     claimAmount: u64,
-}
-
-//event struct
-public struct WealthGodCreated has copy,drop{
-    id:ID,
-    sender:address,
+    wealthGods: vector<address>,
 }
 
 //字段待补充
@@ -39,6 +42,17 @@ public struct WeathPool has key{
     receiver:address,
     sender:address,
     amount:Balance<SUI>,
+}
+
+//event struct
+public struct WealthGodCreated has copy,drop{
+    id:ID,
+    sender:address,
+}
+
+public struct ProfileCreated has copy, drop {
+    id: ID,
+    owner: address,
 }
 
 // sender :create: mint fee :contain 1sui
@@ -52,11 +66,40 @@ fun init(ctx: &mut TxContext){
         sender: ctx.sender(),
         amount:  balance::zero(),
     });
+
+    transfer::share_object(State{
+        id: object::new(ctx),
+        profiles: table::new(ctx),
+    });
 }   
 
+
+
+public entry fun create_profile(
+    state: &mut State,
+    ctx: &mut TxContext,
+) {
+    let uid = object::new(ctx);
+    let owner = ctx.sender();
+    assert!(!table::contains(&state.profiles, owner), EProfileExist);
+    let sendAmount = 0;
+    let claimAmount = 0;
+    let id = object::uid_to_inner(&uid);
+    let new_profile = Profile {
+        id: uid,
+        sendAmount,
+        claimAmount,
+        wealthGods: vector::empty(),
+    };
+    transfer::transfer(new_profile, owner);
+    table::add(&mut state.profiles, owner, object::id_to_address(&id));
+    event::emit(ProfileCreated { id, owner });
+}
+
 //function  发多少钱前端控制
-public entry fun createWealthGod(coin:Coin<SUI>, image: String, user:&mut User,ctx: &mut TxContext) {
-    let amount = coin::into_balance(coin);
+public entry fun createWealthGod(coin:&mut Coin<SUI>, user:&mut Profile,ctx: &mut TxContext) {
+    let in_coin = coin::split(coin, 1000000000, ctx);
+    let amount = coin::into_balance(in_coin);
     let value = amount.value();
     assert!(value > 0,0);
     let uid = object::new(ctx);
@@ -67,19 +110,23 @@ public entry fun createWealthGod(coin:Coin<SUI>, image: String, user:&mut User,c
         sender, 
         isclaimed: false,
         amount,
-        image,
     };
     user.sendAmount = user.sendAmount + value;
     event::emit(WealthGodCreated{
             id,
             sender,
     });
+    vector::push_back(&mut user.wealthGods, object::id_to_address(&id));
     transfer::share_object(wealthGod);
 }
-public entry fun claimWealthGod(wealthGod:&mut WealthGod,amount:&mut Coin<SUI>,random:&Random,pool:&mut WeathPool,user:&mut User,ctx: &mut TxContext){
+
+
+public entry fun claimWealthGod(wealthGod:&mut WealthGod,amount:&mut Coin<SUI>,pool:&mut WeathPool,user:&mut Profile,random:&Random,ctx: &mut TxContext){
          //待改
-        let min:u64  = 3;
-        let max:u64  = 25;
+        assert!(wealthGod.isclaimed == false,1);
+        wealthGod.isclaimed = true;
+        let min:u64  = 300000000;
+        let max:u64  = 2500000000; 
         let mut gen = random::new_generator(random, ctx);
         let claim_amount = random::generate_u64_in_range(&mut gen, min, max);
         //红包钱
@@ -96,4 +143,3 @@ public entry fun claimWealthGod(wealthGod:&mut WealthGod,amount:&mut Coin<SUI>,r
         transfer::public_transfer(sender_coin,pool.sender);
 }
 }
-
