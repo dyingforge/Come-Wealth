6c94fb6e9c8fe43dba44a2e5a38cea443ade274b
Elemen <3242388085@qq.com>
2025-04-21 19:06:45 +0800

    fix

--- web/type/index.ts
+++ web/type/index.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
   export interface DisplayProfile {
       id: {id:string};
       name: string;
@@ -6,15 +6,7 @@
       claimAmount: number;
       wealthGods: WealthGod[];
   }
-=======
-export interface DisplayProfile {
-    id: {id:string};
-    name: string;
-    sendAmount: number;
-    claimAmount: number;
-    wealthGods: WealthGod[];
-}
->>>>>>> 130412369c142ba557a1c9cf849fc2d2f751801f
+
 
   
   export interface LeaderboardItem {
@@ -23,8 +15,6 @@ export interface DisplayProfile {
     amount: number;
   }
   
-
-<<<<<<< HEAD
   export interface Profile {
     id: {id:string};
     name: string;
@@ -56,36 +46,3 @@ export interface DisplayProfile {
       id: string;
       owner: string;
   }
-=======
-export interface Profile {
-  id: {id:string};
-  name: string;
-  sendAmount: number;
-  claimAmount: number;
-  wealthGods: string[];
-}
-
-export interface WealthGod {
-    id: {id:string};
-    sender: string;
-    description: string;
-    isclaimed: boolean;
-    amount: number;
-    claimAmount: number;
-}
-
-export interface State {
-    id: string;
-    profiles: ProfileCreated[];
-}
-
-export interface WealthGodCreated {
-    id: string;
-    sender: string;
-}
-
-export interface ProfileCreated {
-    id: string;
-    owner: string;
-}
->>>>>>> 130412369c142ba557a1c9cf849fc2d2f751801f
