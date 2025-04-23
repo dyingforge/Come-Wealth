286e466c0814801199c76bc7eb47c9145b9fe1fb
dyingforge <1289470395.@qq.com>
2025-04-23 21:58:08 +0800

    [fix] getDisplayProfile

--- web/context/contractsProvider.tsx
+++ web/context/contractsProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { queryProfile,queryState,queryWealthGods,queryWealthGod } from '@/contracts/query'
+import { queryProfile,queryState,queryWealthGods } from '@/contracts/query'
 import { DisplayProfile,WealthGod } from '@/type'
 import { useState } from "react";
 import { useCurrentAccount } from "@mysten/dapp-kit";
@@ -14,6 +14,7 @@ export function ContractsProvider() {
   //查找当前用户的profile
   const getDisplayProfile = async () => {
         const state = await queryState();
+        const wealthGods = await queryWealthGods();
         const userInfo = state.profiles.find((user) => user.owner === currentUser?.address);
         console.log("userInfo", userInfo);
         setHasProfile(!!userInfo);
@@ -25,16 +26,28 @@ export function ContractsProvider() {
               name: profile.name,
               sendAmount: profile.sendAmount,
               claimAmount: profile.claimAmount,
-              wealthGods:[]
-            })as DisplayProfile
-            profile.wealthGods.forEach(async (wealthGodid) => {
-              const wealthGod = await queryWealthGod(wealthGodid)as unknown as WealthGod;
-              displayProfile.wealthGods.push(wealthGod);
-            })
+              wealthGods: []
+            }) as DisplayProfile;
+
+         wealthGods.forEach((wealthGod) => {
+            if (profile.wealthGods.includes(wealthGod.id.id)) {
+              displayProfile.wealthGods.push({
+                id: wealthGod.id,
+                sender: wealthGod.sender,
+                description: wealthGod.description,
+                isclaimed: wealthGod.isclaimed,
+                claimAmount: wealthGod.claimAmount,
+                coin_type: wealthGod.coin_type,
+                coin: wealthGod.coin
+              });
+            }
+          });
+            setHasProfile(true);
             setUserProfile(displayProfile);
-            return displayProfile;
+            return displayProfile as DisplayProfile;
         }
-    };
+       
+      };
  
 
   //查找所有财富神
